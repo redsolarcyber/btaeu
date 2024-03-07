ab4e03e94a5e5f144ad24843d21510bd10b0d6b1
redsolarcyber <157942922+redsolarcyber@users.noreply.github.com>
2024-03-07 10:19:33 +0700

    Add files via upload

---
+++ btaeu.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+echo "Assalamualaikum B0"
+echo "Please Insert The Number:"
+read number
+
+case $number in
+    +6282111951044)
+        echo "Name: MUHAMMAD AL FAZIL ASSUBKI"
+        echo "NIK: Unknown"
+        echo "YOB: 2010"
+        echo "Address: Keudè Bayu"
+        echo "School: SMP 11 LHOKSEUMAWE"
+        echo "Bank Number: Unknown"
+        ;;
+    +6285371667042)
+        echo "Name: Yuswani"
+        echo "NIK: 11782763289938"
+        echo "YOB: Unknown"
+        echo "Address: Alue Awe"
+        echo "School: Unknown"
+        echo "Bank Number: Unknown"
+        ;;
+    +6283172360894)
+        echo "Name: Ruslan"
+        echo "NIK: 117827632881729"
+        echo "YOB: Unknown"
+        echo "Address: Buket Rata"
+        echo "School: Unknown"
+        echo "Bank Number: Unknown"
+        ;;
+    +6283872875214)
+        echo "Name: Afrida"
+        echo "NIK: 117827672628729"
+        echo "YOB: Unknown"
+        echo "Address: Blang Pria"
+        echo "School: Unknown"
+        echo "Bank Number: Unknown"
+        ;;
+    +6285277493038)
+        echo "Name: Aisyah"
+        echo "NIK: 11782763272929"
+        echo "YOB: Unknown"
+        echo "Address: Baloi"
+        echo "School: Unknown"
+        echo "Bank Number: Unknown"
+        ;;
+    *)
+        echo "Number not found"
+        ;;
+esac
